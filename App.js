1d9a963f545b5eacb3cbf91e44f71893ae20e029
ankush t <ankushtripathi510@gmail.com>
2024-01-21 16:07:24 +0530

    react method

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", { id: "el" }, "Hello react")));
+console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(root);
+root.render(parent);
